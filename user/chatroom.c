c30c283aff0410630699485c8a25c3f40ac8af65
acd7 <acd7@pyrite-n5.cs.iastate.edu>
2025-02-17 17:33:57 -0600

    Got the chatroom a lot closer to working, but still having issues with parent command interference.

--- user/chatroom.c
+++ user/chatroom.c
@@ -49,8 +49,27 @@ gets1(char msgBuf[MAX_MSG_LEN]){
 	msgBuf[len-1]='\0';
 }
 
-// Function to extract substring after a specific character
-void extractSubstringAfterChar(const char *input, char target, char *output) {
+// Function to extract the message before the character '!'
+void extractMessage(const char *input, char *output) {
+    char delimiter = '!';
+    int i = 0, j = 0;
+
+    // Iterate through input string
+    while (input[i] != '\0') {
+        if (input[i] == delimiter) {
+            break; // Stop copying at the delimiter
+        }
+        output[j++] = input[i];
+        i++;
+    }
+
+    output[j] = '\0'; // Null-terminate the output string
+}
+
+
+// Function to extract the bot's name after the character '!'
+void extractName(const char *input, char *output) {
+    char delimiter = '!';
     int i = 0, j = 0;
     int found = 0;
 
@@ -60,7 +79,7 @@ void extractSubstringAfterChar(const char *input, char target, char *output) {
             // Copy characters after target character
             output[j++] = input[i];
         }
-        if (input[i] == target) {
+        if (input[i] == delimiter) {
             found = 1; // Start copying from next character
         }
         i++;
@@ -68,6 +87,25 @@ void extractSubstringAfterChar(const char *input, char target, char *output) {
 
     output[j] = '\0'; // Null-terminate the output string
 }
+
+// int strlen(const char *s) {
+//     int len = 0;
+//     while (s[len] != '\0') {
+//         len++;
+//     }
+//     return len;
+// }
+
+// strcat function, which appends src to dest
+char *strcat(char *dest, const char *src) {
+    char *ptr = dest + strlen(dest);
+    while (*src) {
+        *ptr++ = *src++;
+    }
+    *ptr = '\0';
+    return dest;
+}
+
 
 /* NOTE: the reworking of the chatbot function isn't complete, but it was
 meant to be a simple way to swith between bots without the need to fully strip 
@@ -84,6 +122,7 @@ see what I was going for.
 void
 chatbot(int myId, char *myName)
 {
+
     //close un-used pipe descriptors
     for(int i=0; i<myId-1; i++){
         close(fd[i][0]);
@@ -92,11 +131,31 @@ chatbot(int myId, char *myName)
     close(fd[myId-1][1]);
     close(fd[myId][0]);
 
+    //to receive msg from the previous chatbot
+    char recvMsg[MAX_MSG_LEN];
+    read(fd[myId-1][0], recvMsg, MAX_MSG_LEN);
+
+    char recvdBotName[MAX_MSG_LEN];
+    extractName(recvMsg, recvdBotName);
+
+    // Test: Print received message, extracted bot name, my name, and my ID
+    printf("Outside loop\n");
+    printf("Received message: %s\n", recvMsg);
+    printf("Extracted bot name: %s\n", recvdBotName);
+    printf("My name: %s\n", myName);
+    printf("My ID: %d\n", myId);
+
+    // If the received bot name is not the same as the current bot's name, 
+    // pass the message to the next bot
+    if (strcmp(recvdBotName, myName) != 0) {
+        write(fd[myId][1], recvMsg, MAX_MSG_LEN);
+        exit(0);
+    }
+
     //loop
     while(1){
         //to get msg from the previous chatbot (in loop for now)
-        char recvMsg[MAX_MSG_LEN];
-        read(fd[myId-1][0], recvMsg, MAX_MSG_LEN);
+        
 
         // // if the name after the character [ is not the same as the bot name, go to the next bot
         // char botName[MAX_MSG_LEN];
@@ -128,6 +187,14 @@ chatbot(int myId, char *myName)
                         char newBot[MAX_MSG_LEN];
                         gets1(newBot);
 
+                        // Test: Print new bot, received message, extracted bot name, my name, and my ID
+                        printf("Inside loop\n");
+                        printf("New bot: %s\n", newBot);
+                        printf("Received message: %s\n", recvMsg);
+                        printf("Extracted bot name: %s\n", recvdBotName);
+                        printf("My name: %s\n", myName);
+                        printf("My ID: %d\n", myId);
+
                         // Find the index of the new bot (successor)
                         for (int i = 0; i < botCount; i++) {
                             if (strcmp(newBot, botNames[i]) == 0) {
@@ -137,15 +204,18 @@ chatbot(int myId, char *myName)
                         }
 
                         // If bot exists and is the current bot, continue chat
-                        if (newBotIndex == myId - 1) {
+                        if (strcmp(myName, newBot) == 0) {
                             printf("You are already chatting with %s.\n", botNames[newBotIndex]);
                             newBotIndex = -1; // Reset for next change
                             break;
                         }
 
+                        // If bot exists and is not the current bot, switch
                         if (newBotIndex != -1 && newBotIndex != myId - 1) {
                             //pass the msg to the next bot with a name added
                             newBotIndex = -1; // Reset for next change
+                            strcat(msgBuf, "!");
+                            strcat(msgBuf, newBot);
                             write(fd[myId][1], msgBuf, MAX_MSG_LEN); 
                             break;
                         }
@@ -183,6 +253,15 @@ main(int argc, char *argv[])
         exit(1);
     }
 
+    // // Testing message and name extraction
+    // char testMsg[MAX_MSG_LEN] = "Message/command!Alice";
+    // char extractedMsg[MAX_MSG_LEN];
+    // char extractedName[MAX_MSG_LEN];
+    // extractMessage(testMsg, extractedMsg);
+    // extractName(testMsg, extractedName);
+    // printf("Extracted message: %s\n", extractedMsg);
+    // printf("Extracted name: %s\n", extractedName);
+
     // Bot count
     botCount = argc - 1;
 
@@ -209,8 +288,31 @@ main(int argc, char *argv[])
         close(fd[i][1]);
     }
 
-    //send the START msg to the first chatbot
-    write(fd[0][1], ":START", 6);
+    //FIX THIS, THIS IS WHERE THE PROBLEM IS
+    // String to hold START!
+    char startMsg[MAX_MSG_LEN] = ":START!";
+    // Add the first bot name to the string
+    strcat(startMsg, argv[1]);
+
+    // Get length of the whole string
+    int len = strlen(startMsg);
+
+    // // Test: Print the start message
+    // printf("Start message: %s\n", startMsg);
+
+    // // Test: extract command from message
+    // char extractedMsg[MAX_MSG_LEN];
+    // extractMessage(startMsg, extractedMsg);
+    // printf("Extracted message: %s\n", extractedMsg);
+
+    // // Test: extract name from message
+    // char extractedName[MAX_MSG_LEN];
+    // extractName(startMsg, extractedName);
+    // printf("Extracted name: %s\n", extractedName);
+
+
+    //send START![firstBotName] to the first bot
+    write(fd[0][1], startMsg, len);
 
     //loop: when receive a token from predecessor, pass it to successor
     while(1) {
