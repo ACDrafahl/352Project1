98cf464c7d817e17093e450a6571d444d501e0cd
acd7 <acd7@pyrite-n3.cs.iastate.edu>
2025-02-16 19:50:35 -0600

    Added an array to store bot names and an index for the current bot.

--- user/chatroom.c
+++ user/chatroom.c
@@ -4,10 +4,11 @@
 #include "user/user.h"
 
 #define MAX_MSG_LEN 512
-#define MAX_NUM_CHATBOT 10
+#define MAX_NUM_CHATBOT 5
 
-int fd[MAX_NUM_CHATBOT+1][2];
-
+int fd[MAX_NUM_CHATBOT+1][2]; // Pipes for chatbots
+char *botNames[MAX_NUM_CHATBOT+1]; // Store bot names
+int currentbot = 0; // Current bot index
 
 //handle exception
 void
@@ -60,17 +61,17 @@ chatbot(int myId, char *myName)
     close(fd[myId-1][1]);
     close(fd[myId][0]);
 
-    //to get msg from the previous chatbot (relocated to avoid redundancy)
-    char recvMsg[MAX_MSG_LEN];
+    
 
     //loop
     while(1){
+        //to get msg from the previous chatbot (in loop for now)
+        char recvMsg[MAX_MSG_LEN];
         read(fd[myId-1][0], recvMsg, MAX_MSG_LEN);
 
 	    if(strcmp(recvMsg,":EXIT")!=0 && strcmp(recvMsg,":exit")!=0){//if the received msg is not EXIT/exit: continue chatting 
             
 	        printf("Hello, this is chatbot %s. Please type:\n", myName);
-            
 
             while (1) { // keep chatting until user enters :CHANGE/:change or :EXIT/:exit
                 //get a string from std input and save it to msgBuf 
@@ -95,8 +96,6 @@ chatbot(int myId, char *myName)
 
                 printf("Please continue typing:\n");
             }
-
-	        
 
         }else{//if receives EXIT/exit: pass the msg down and exit myself
             write(fd[myId][1], recvMsg, MAX_MSG_LEN);
